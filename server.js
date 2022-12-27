cfd79869a1bc46886c8d67e4c753c5f882a4b441
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-12-26 21:19:44 -0500

    working api

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = 5025
+const MongoClient = require ('mongodb').MongoClient
+const PORT = 5050
+require('dotenv').config()
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'parksideVirtualWalk'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cors())
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-})
+
 
 const parks = {
     'arches':{
@@ -42,6 +56,44 @@ const parks = {
     }
 
 }
+
+// app.get('/', (request, response) => {
+//     response.sendFile(__dirname + '/index.html')
+// })
+
+app.get('/',(request, response)=>{
+    db.collection('parks').find().sort({likes: -1}).toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
+
+
+app.post('/addPark', (request, response) => {
+    db.collection('parks').insertOne({parkName: request.body.parkName,
+    parkLocation: request.body.parkLocation, parkVideo: request.body.parkVideo, likes: 0})
+    .then(result => {
+        console.log('Park Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/api', (request, response) => {
     response.json(parks)
